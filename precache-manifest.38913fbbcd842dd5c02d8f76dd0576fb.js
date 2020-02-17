e4accf5bf3d30b18a2360bec0b1ae7a37526c084
Tim Shaker <tim@tmshkr.com>
2020-02-17 11:33:49 -0800

    Updates

---
+++ precache-manifest.38913fbbcd842dd5c02d8f76dd0576fb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34ed1e5682f3d2828634e65496d2a25e",
+    "url": "/react-american-football-scoreboard/index.html"
+  },
+  {
+    "revision": "f2388e50ab338d81288a",
+    "url": "/react-american-football-scoreboard/static/css/main.bc36587d.chunk.css"
+  },
+  {
+    "revision": "d95fe823498b4d7c1c7b",
+    "url": "/react-american-football-scoreboard/static/js/2.cdb8e493.chunk.js"
+  },
+  {
+    "revision": "f2388e50ab338d81288a",
+    "url": "/react-american-football-scoreboard/static/js/main.a7ef08c5.chunk.js"
+  },
+  {
+    "revision": "916aa41878aeb9a7ff75",
+    "url": "/react-american-football-scoreboard/static/js/runtime~main.521baad1.js"
+  },
+  {
+    "revision": "1abd8c1a58598974d8415573764ba76a",
+    "url": "/react-american-football-scoreboard/static/media/Ozone.1abd8c1a.ttf"
+  }
+]);
